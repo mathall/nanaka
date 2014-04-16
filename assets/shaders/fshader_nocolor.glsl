e4f1e3e89989fcf933f1a946ec025d4c4c823be7
Mathias Hällman <mathias87@gmail.com>
2014-04-16 19:50:07 +0200

    Only specify precision in shader when on GLES.

--- assets/shaders/fshader_nocolor.glsl
+++ assets/shaders/fshader_nocolor.glsl
@@ -1,4 +1,6 @@
+#ifdef GL_ES
 precision mediump float;
+#endif // GL_ES
 varying vec2 Texcoord;
 varying vec4 outColor;
 uniform sampler2D tex;
--- assets/shaders/fshader_notex.glsl
+++ assets/shaders/fshader_notex.glsl
@@ -1,4 +1,6 @@
+#ifdef GL_ES
 precision mediump float;
+#endif // GL_ES
 uniform vec4 color;
 varying vec2 Texcoord;
 varying vec4 outColor;
